b16bc7c3ab8711a1ba00cfcabac886e3020d4b4d
Mateusz Wojtyna <w_mateusz@hotmail.com>
2024-02-20 05:13:30 +0100

    feat: add wakatime

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -559,6 +559,11 @@ require("lazy").setup({
     opts = {},
   },
 
+  {
+    "wakatime/vim-wakatime",
+    event = "BufRead",
+  },
+
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
