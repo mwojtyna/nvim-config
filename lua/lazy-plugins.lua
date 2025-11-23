adb02f494caf8aaa10b29a137971df43213a5547
Mateusz Wojtyna <w_mateusz@hotmail.com>
2025-11-23 22:11:26 +0100

    feat: autopairs

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -3,6 +3,21 @@
 --    as they will be available in your neovim runtime.
 require("lazy").setup({
   {
+    "windwp/nvim-autopairs",
+    -- Optional dependency
+    dependencies = { "hrsh7th/nvim-cmp" },
+    config = function()
+      local npairs = require("nvim-autopairs")
+      npairs.setup({})
+
+      -- If you want to automatically add `(` after selecting a function or method
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      local cmp = require("cmp")
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end,
+  },
+
+  {
     "tpope/vim-fugitive",
     cmd = { "Git", "Gdiffsplit", "Gvdiffsplit" },
     keys = {
